6008400850f1b2a773549957dcbc9ffa275a2f46
Herbert Nícolas <herbert.nicolas@fabricadenegocio.com.br>
2024-12-09 23:21:35 -0300

    init: basic backend nodejs server created

---
+++ back-nodejs/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// rota basica
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// iniciando o server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}!!! 🚀`);
+});
