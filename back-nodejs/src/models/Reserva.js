95a1f765c3553e45e0c0c8e2a20e230880f456f3
herbert <herbert.nicolas@fabricadenegocio.com.br>
2025-02-11 19:29:20 -0300

    feat: reserva model and controller added

--- back-nodejs/src/models/Reserva.js
+++ back-nodejs/src/models/Reserva.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const ReservaSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
+  equipSalaId: { type: mongoose.Schema.Types.ObjectId, ref: 'EquipSala', required: true },
+  salaId: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: false },
+  tipo: { type: String, required: true },
   dataReserva: { type: String, required: false },
-  status: { type: String, required: false },
+  usuarioId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true } 
 });
 
 const Reserva = mongoose.model('Reserva', ReservaSchema);
