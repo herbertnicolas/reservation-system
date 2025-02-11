61f6aa7784dcbb61874af96e168b17c0d274d6ab
herbert <herbert.nicolas@fabricadenegocio.com.br>
2025-02-11 19:32:38 -0300

    chore: changed GerEquipSala to EquipSala

--- back-nodejs/src/models/EquipSala.js
+++ back-nodejs/src/models/EquipSala.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-const GerEquipSalaSchema = new mongoose.Schema({
+const EquipSalaSchema = new mongoose.Schema({
   salaId: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
   equipamentoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Equipamento', required: true },
-  quantidade: { type: Number, required: false, min: 0 }
+  quantidade: { type: Number, required: false, min: 0 },
+  datasReservas: { type: [String], default: [] }, 
 });
 
-module.exports = mongoose.model('GerEquipSala', GerEquipSalaSchema);
+module.exports = mongoose.model('EquipSala', EquipSalaSchema);
