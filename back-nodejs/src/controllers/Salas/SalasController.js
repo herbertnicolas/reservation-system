18ced9100a74146a34e9aa2dcc3bae4427c28769
Gabriel Stamford <gabrielstamford21@hotmail.com>
2025-02-14 21:00:18 -0300

    test: adding functions on rooms tests

--- back-nodejs/src/controllers/Salas/SalasController.js
+++ back-nodejs/src/controllers/Salas/SalasController.js
@@ -9,39 +9,71 @@ const getRooms = async (req, resp) => {
     }
 }
 
-const getRoom = async (req, resp) => {
+const getRoom = async (req, res) => {
+    const { id } = req.params;
+  
     try {
-        const { id } = req.params;
-        const room = await Room.findById(id);
-        resp.status(200).json(room);
+      const room = await Room.findOne({ identificador: id }); // Busca pelo identificador
+      if (!room) {
+        return res.status(404).json({
+          status: "Not Found",
+          mensagem: `Sala com identificador ${id} não encontrada!`,
+        });
+      }
+  
+      res.status(200).json({
+        mensagem: "Sala encontrada!",
+        sala: {
+          identificador: room.identificador,
+          localizacao: room.localizacao,
+          capacidade: room.capacidade,
+        },
+      });
     } catch (error) {
-        resp.status(500).json({message: error.message});
+      res.status(500).json({
+        status: "Internal Server Error",
+        mensagem: "Erro ao buscar sala",
+      });
     }
-}
+};
 
 const createRoom = async (req, res) => {
     const { identificador, localizacao, capacidade } = req.body;
-    const existingRoom = await Room.findOne({ identificador });
+    if (typeof identificador !== 'string') {
+      return res.status(400).json({
+        erro: "Identificador deve ser uma string!",
+      });
+    }
+  
+    if (typeof capacidade !== 'number') {
+      return res.status(400).json({
+        erro: "Capacidade deve ser um número!",
+      });
+    }
+
+    const existingRoom = await Room.findOne({ identificador, localizacao, capacidade });
     if (existingRoom) {
-        return res.status(400).json({ erro: "Sala com identificador já existe!" });
+      return res.status(400).json({
+        erro: `Sala com identificador ${identificador}, localização ${localizacao} e capacidade ${capacidade} já existe!`,
+      });
     }
-    if (typeof identificador !== 'string' || typeof capacidade !== 'number') {
-        return res.status(400).json({ erro: "Dados inválidos!" });
-    }
+  
     try {
-        const newRoom = await Room.create({ identificador, localizacao, capacidade });
-        res.status(201).json({
+      const newRoom = await Room.create({ identificador, localizacao, capacidade });
+      res.status(201).json({
         mensagem: "Sala criada com sucesso!",
         sala: {
-            identificador: newRoom.identificador,
-            localizacao: newRoom.localizacao,
-            capacidade: newRoom.capacidade,
+          identificador: newRoom.identificador,
+          localizacao: newRoom.localizacao,
+          capacidade: newRoom.capacidade,
         },
-        });
+      });
     } catch (error) {
-        res.status(500).json({ erro: "Erro ao criar sala" });
+      res.status(500).json({
+        erro: "Erro ao criar sala",
+      });
     }
-}
+};
 
 const updateRoom = async (req, resp) => {
     try {
@@ -52,15 +84,12 @@ const updateRoom = async (req, resp) => {
         if (!room){
             return resp.status(404).json({message: `Sala com identificador ${id} não encontrada`});
         }
-
         const Updatedroom = await Room.findById(id);
         resp.status(200).json(Updatedroom);
-
     } catch (error) {
         resp.status(500).json({message: error.message});
     }
 }
-
 const deleteRoom = async (req, resp) => {
     try {
         const { id } = req.params;
@@ -74,7 +103,6 @@ const deleteRoom = async (req, resp) => {
         resp.status(500).json({message: error.message});
     }
 }
-
 module.exports = {
     getRooms,
     getRoom,
