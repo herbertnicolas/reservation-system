4bd43b0cdd0505a8d0d3e705d7b60a87535032ab
Raquel Carneiro <raquelrocarneiro@gmail.com>
2025-02-16 00:23:08 -0300

    Add: parte da verificação das reservas + alterações no model reserva e no server

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+//arquivos de rotas
+const roomRoutes = require('./routes/salas.routes.js');
+const equipamentosRoutes = require('./routes/equipamentos.routes.js');
+const equipSalaRoutes = require('./routes/equipsala.routes.js');
+const reservasRoutes = require('./routes/reservas.routes.js');
+const verificarReservasRoutes = require('./routes/verificarreservas.routes.js');
+
+const { connectDB } = require('./database/index');
+const app = express();
+
+// iniciando o banco de dados
+(async () => {
+  try {
+    await connectDB(process.env.MONGODB_URI);
+  } catch (err) {
+    console.error('Erro ao conectar ao banco de dados', err);
+  }
+})();
+
+// iniciando as rotas
+app.use(express.json());
+
+// iniciando as rotas
+app.use('/salas', roomRoutes);
+app.use('/equipamentos', equipamentosRoutes);
+app.use('/equipsala', equipSalaRoutes);
+app.use('/reservas', reservasRoutes);
+app.use('/verificarreservas', verificarReservasRoutes);
+
+// iniciando o server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}!!! 🚀`);
+});
+
+module.exports = { app };
