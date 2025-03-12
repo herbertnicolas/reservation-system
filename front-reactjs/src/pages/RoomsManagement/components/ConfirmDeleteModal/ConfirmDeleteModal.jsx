87d82f73f5f6870a9d1a09ac7a33884fc182dde8
herbert <herbert.nicolas@fabricadenegocio.com.br>
2025-03-12 20:47:35 -0300

    feat: added initial RoomsManagement page and styles

---
+++ front-reactjs/src/pages/RoomsManagement/components/ConfirmDeleteModal/ConfirmDeleteModal.jsx
@@ -0,0 +1,44 @@
+import { Button } from "../../../../components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogOverlay,
+  DialogTitle,
+} from "../../../../components/ui/dialog";
+
+export function ConfirmDeleteModal({ isOpen, onClose, onConfirm }) {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogOverlay className="bg-white bg-opacity-50" />
+      <DialogContent className="flex items-center justify-center bg-white bg-opacity-50">
+        <div className="bg-white p-6 rounded-md shadow-md">
+          <DialogTitle className="text-lg font-semibold">
+            Deseja realmente remover o usuário?
+          </DialogTitle>
+          <DialogDescription className="mt-2 text-sm text-gray-600">
+            Essa ação não poderá ser desfeita. Ao clicar em ‘Remover’, o usuário
+            cadastrado será removido da plataforma por completo.
+          </DialogDescription>
+          <div className="mt-4 flex justify-end space-x-2">
+            <Button variant="outline" onClick={onClose}>
+              Cancelar
+            </Button>
+            <Button
+              style={{
+                backgroundColor: "#000",
+                color: "#fff",
+                width: "20%",
+                height: "40px",
+              }}
+              type="primary"
+              onClick={onConfirm}
+            >
+              Remover
+            </Button>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
